ebc059e510113b4ec168b1ee7f9ec531fb3541e3
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-04-23 00:25:03 -0400

    test in episodes

--- src/App.test.js
+++ src/App.test.js
@@ -8,15 +8,16 @@ jest.mock('./api/fetchShow');
 console.log("mockFetchShow: ", mockFetchShow);
 
 
-test("App fetches data and renders properly", async () => {
+test("App fetches data properly", async () => {
     const mockData = {
-        id: '123',
-        image: { medium: 'medium_image'},
-        name: 'name',
-        season: 3,
-        number: 2,
-        summary: '<p>Summary</p>',
-        runtime: 20
+        id: '909346',
+        image: { medium: "http://static.tvmaze.com/uploads/images/medium_landscape/132/332052.jpg" },
+        name: 'Chapter Six: The Spy',
+        number: 6,
+        runtime: 60,
+        season: 2,
+        summary: '<p>Plot Summary</p>',
+        url: "http://www.tvmaze.com/episodes/909346/stranger-things-2x06-chapter-six-the-spy"
     }
     mockFetchShow.mockResolvedValue(mockData);
     const { queryAllByText } = render(<App />);
